36edc4b108708144dd4a9e8ebf77fe8de8465c7c
dudupfer-prog <dudupfer@gmail.com>
2026-08-08 11:38:18 -0300

    Add null check for camera in rotateParticle method

--- src/main/java/net/id107/flexfov/mixin/BillboardParticleMixin.java
+++ src/main/java/net/id107/flexfov/mixin/BillboardParticleMixin.java
@@ -8,11 +8,11 @@ import net.id107.flexfov.projection.Projection;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.particle.BillboardParticle;
 import net.minecraft.client.particle.Particle;
-import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3f;
 
 @Mixin(value = BillboardParticle.class, priority = 1500)
 public abstract class BillboardParticleMixin extends Particle {
@@ -29,12 +29,14 @@ public abstract class BillboardParticleMixin extends Particle {
 		if (Projection.getProjection().shouldRotateParticles()) {
 			MinecraftClient mc = MinecraftClient.getInstance();
 			Entity camera = mc.cameraEntity;
-			Vec3d cameraPos = camera.getPos().subtract(camera.prevX, camera.prevY, camera.prevZ).multiply(Projection.getTickDelta()).add(new Vec3d(camera.prevX, camera.prevY, camera.prevZ));
-			Vec3d particlePos = new Vec3d(x, y, z).subtract(new Vec3d(prevPosX, prevPosY, prevPosZ)).multiply(Projection.getTickDelta()).add(new Vec3d(prevPosX, prevPosY, prevPosZ));
-			Vec3d dir = cameraPos.subtract(particlePos).normalize();
-			quaternion.set(0, 0, 0, 1);
-			quaternion.hamiltonProduct(Vector3f.POSITIVE_Y.getRadialQuaternion((float)Math.atan2(-dir.x, -dir.z)));
-			quaternion.hamiltonProduct(Vector3f.POSITIVE_X.getRadialQuaternion((float)Math.asin(dir.y)));
+			if (camera != null) {
+				Vec3d cameraPos = camera.getPos().subtract(camera.prevX, camera.prevY, camera.prevZ).multiply(Projection.getTickDelta()).add(new Vec3d(camera.prevX, camera.prevY, camera.prevZ));
+				Vec3d particlePos = new Vec3d(x, y, z).subtract(new Vec3d(prevPosX, prevPosY, prevPosZ)).multiply(Projection.getTickDelta()).add(new Vec3d(prevPosX, prevPosY, prevPosZ));
+				Vec3d dir = cameraPos.subtract(particlePos).normalize();
+				quaternion.set(0, 0, 0, 1);
+				quaternion.hamiltonProduct(Vec3f.POSITIVE_Y.getRadialQuaternion((float)Math.atan2(-dir.x, -dir.z)));
+				quaternion.hamiltonProduct(Vec3f.POSITIVE_X.getRadialQuaternion((float)Math.asin(dir.y)));
+			}
 		}
 		return quaternion;
 	}
