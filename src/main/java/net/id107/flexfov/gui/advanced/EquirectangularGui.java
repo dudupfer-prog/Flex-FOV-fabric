8b5c0140eeea1eb1c0ddc6f1f37e07ab428ad0db
dudupfer-prog <dudupfer@gmail.com>
2026-08-08 11:51:04 -0300

    Replace LiteralText with Text for button messages

--- src/main/java/net/id107/flexfov/gui/advanced/EquirectangularGui.java
+++ src/main/java/net/id107/flexfov/gui/advanced/EquirectangularGui.java
@@ -5,7 +5,7 @@ import net.id107.flexfov.projection.Equirectangular;
 import net.id107.flexfov.projection.Projection;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 
 public class EquirectangularGui extends AdvancedGui {
 
@@ -18,22 +18,22 @@ public class EquirectangularGui extends AdvancedGui {
 	protected void init() {
 		super.init();
 		
-		addButton(new ButtonWidget(width / 2 - 155, height / 6 + 84, 150, 20,
-				new LiteralText("Show Circle: " + (Equirectangular.drawCircle ? "ON" : "OFF")), (buttonWidget) -> {
+		addDrawableChild(new ButtonWidget(width / 2 - 155, height / 6 + 84, 150, 20,
+				Text.literal("Show Circle: " + (Equirectangular.drawCircle ? "ON" : "OFF")), (buttonWidget) -> {
 					Equirectangular.drawCircle = !Equirectangular.drawCircle;
-					buttonWidget.setMessage(new LiteralText("Show Circle: " + (Equirectangular.drawCircle ? "ON" : "OFF")));
+					buttonWidget.setMessage(Text.literal("Show Circle: " + (Equirectangular.drawCircle ? "ON" : "OFF")));
 					ConfigManager.saveConfig();
 				}));
-		addButton(new ButtonWidget(width / 2 - 155, height / 6 + 132, 150, 20,
-				new LiteralText("Stabilize Pitch: " + (Equirectangular.stabilizePitch ? "ON" : "OFF")), (buttonWidget) -> {
+		addDrawableChild(new ButtonWidget(width / 2 - 155, height / 6 + 132, 150, 20,
+				Text.literal("Stabilize Pitch: " + (Equirectangular.stabilizePitch ? "ON" : "OFF")), (buttonWidget) -> {
 					Equirectangular.stabilizePitch = !Equirectangular.stabilizePitch;
-					buttonWidget.setMessage(new LiteralText("Stabilize Pitch: " + (Equirectangular.stabilizePitch ? "ON" : "OFF")));
+					buttonWidget.setMessage(Text.literal("Stabilize Pitch: " + (Equirectangular.stabilizePitch ? "ON" : "OFF")));
 					ConfigManager.saveConfig();
 				}));
-		addButton(new ButtonWidget(width / 2 + 5, height / 6 + 132, 150, 20,
-				new LiteralText("Stabilize Yaw: " + (Equirectangular.stabilizeYaw ? "ON" : "OFF")), (buttonWidget) -> {
+		addDrawableChild(new ButtonWidget(width / 2 + 5, height / 6 + 132, 150, 20,
+				Text.literal("Stabilize Yaw: " + (Equirectangular.stabilizeYaw ? "ON" : "OFF")), (buttonWidget) -> {
 					Equirectangular.stabilizeYaw = !Equirectangular.stabilizeYaw;
-					buttonWidget.setMessage(new LiteralText("Stabilize Yaw: " + (Equirectangular.stabilizeYaw ? "ON" : "OFF")));
+					buttonWidget.setMessage(Text.literal("Stabilize Yaw: " + (Equirectangular.stabilizeYaw ? "ON" : "OFF")));
 					ConfigManager.saveConfig();
 				}));
 	}
